b592b3a916850c7b279498a0f3a1e2cec1efd6b4
Volkmar Rigo <volkmar@rigo.bz>
2021-02-21 18:05:10 +0100

    First Attempt

--- GoogleHashCode/Algorithms/Solver2.cs
+++ GoogleHashCode/Algorithms/Solver2.cs
@@ -1,13 +1,35 @@
 ﻿using GoogleHashCode.Base;
 using GoogleHashCode.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GoogleHashCode.Algorithms
 {
-	public class Solver2 : ISolver<Input, Output>
-	{
-		public Output Solve(Input input)
-		{
-			return new();
-		}
-	}
+    public class Solver2 : ISolver<Input, Output>
+    {
+        List<Pizza> Pizzas = new();
+
+        public Output Solve(Input input)
+        {
+            Pizzas = input.Pizzas.OrderByDescending(q => q.ingredients.Count).ToList();
+            var pizzaStart = 0;
+
+            var result = new Output();
+            foreach (var team in input.NumberOfTeams.OrderByDescending(q => q.Key))
+            {
+                for (var teamIndex = 0; teamIndex < team.Value; teamIndex++)
+                {
+                    var pizzaIds = Pizzas.Skip(pizzaStart).Take(team.Key).Select(q => q.id).ToList();
+                    if (pizzaIds.Count != team.Key)
+                        break;
+
+                    result.Deliveries.Add(new Delivery(team.Key, pizzaIds));
+
+                    pizzaStart += team.Key;
+                }
+            }
+
+            return result;
+        }
+    }
 }
