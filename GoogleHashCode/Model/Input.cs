847e150a3df6d6d79a6cbb429a56957816253299
Volkmar Rigo <volkmar@rigo.bz>
2021-02-21 17:45:47 +0100

    Input 2

--- GoogleHashCode/Model/Input.cs
+++ GoogleHashCode/Model/Input.cs
@@ -3,23 +3,26 @@ using System.Linq;
 
 namespace GoogleHashCode.Model
 {
+	public record Pizza(int id, HashSet<string> ingredients);
+
 	public record Input
 	{
 		public int NumberOfPizzas { get; set; }
 		public SortedDictionary<int, int> NumberOfTeams { get; set; } = new();
-		public List<HashSet<string>> Pizzas = new();
+		public List<Pizza> Pizzas = new();
 
-		static HashSet<string> ParsePizza(string line)
-			=> line.Split(' ').Skip(1).ToHashSet();
+		static Pizza ParsePizza(int id, string line)
+			=> new Pizza(id, line.Split(' ').Skip(1).ToHashSet());
 
 		public static Input Parse(string[] values)
 		{
 			var first = values[0].Split(' ', System.StringSplitOptions.RemoveEmptyEntries).Select(q => int.Parse(q)).ToArray();
+			var id = 0;
 			var result = new Input
 			{
 				NumberOfPizzas = first[0],
 				NumberOfTeams = new SortedDictionary<int, int> { { 2, first[1] }, { 3, first[2] }, { 4, first[3] } },
-				Pizzas = values.Skip(1).Select(q => ParsePizza(q)).ToList()
+				Pizzas = values.Skip(1).Select(q => ParsePizza(id++, q)).ToList()
 			};
 
 			return result;
