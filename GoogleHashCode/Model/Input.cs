4b6fada6f7cf31f2a5a0a761215372e398c97633
Volkmar Rigo <volkmar@rigo.bz>
2021-02-21 17:21:50 +0100

    input

--- GoogleHashCode/Model/Input.cs
+++ GoogleHashCode/Model/Input.cs
@@ -1,10 +1,28 @@
-﻿namespace GoogleHashCode.Model
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GoogleHashCode.Model
 {
 	public record Input
 	{
+		public int NumberOfPizzas { get; set; }
+		public SortedDictionary<int, int> NumberOfTeams { get; set; } = new();
+		public List<HashSet<string>> Pizzas = new();
+
+		static HashSet<string> ParsePizza(string line)
+			=> line.Split(' ').Skip(1).ToHashSet();
+
 		public static Input Parse(string[] values)
 		{
-			return new();
+			var first = values[0].Split(' ', System.StringSplitOptions.RemoveEmptyEntries).Select(q => int.Parse(q)).ToArray();
+			var result = new Input
+			{
+				NumberOfPizzas = first[0],
+				NumberOfTeams = new SortedDictionary<int, int> { { 2, first[1] }, { 3, first[2] }, { 4, first[3] } },
+				Pizzas = values.Skip(1).Select(q => ParsePizza(q)).ToList()
+			};
+
+			return result;
 		}
 	}
 }
