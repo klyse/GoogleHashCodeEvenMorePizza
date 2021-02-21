d70fd9e17f64f4523d74a555a16bebf6efa86808
Volkmar Rigo <volkmar@rigo.bz>
2021-02-21 17:31:24 +0100

    Output, first version

--- GoogleHashCode/Model/Output.cs
+++ GoogleHashCode/Model/Output.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using GoogleHashCode.Base;
 
 namespace GoogleHashCode.Model
 {
+	public record Delivery(int teamSize, List<int> pizzaIds);
+
+
 	public record Output : IOutput
 	{
+		public List<Delivery> Deliveries = new List<Delivery>();
+
 		public string[] GetOutputFormat()
 		{
-			throw new NotImplementedException();
+			var result = new List<string>();
+			result.Add(Deliveries.Count.ToString());
+			foreach (var item in Deliveries)
+				result.Add($"{item.teamSize} {string.Join(' ', item.pizzaIds)}");
+
+			return result.ToArray();
 		}
 
 		public int GetScore()
 		{
-			throw new NotImplementedException();
+			return 0;
+			// throw new NotImplementedException();
 		}
 	}
 }
